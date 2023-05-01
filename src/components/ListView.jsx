125a4449789ab95aa062dbbe2ba684ef7161de5e
Corrie Stroup <Corrie.Stroup@gmail.com>
2023-05-01 17:43:29 +0700

    Added dynamic sorting
    Sorting can go up and down now, title and released now have an indicator that doesn't quite work right

--- src/components/ListView.jsx
+++ src/components/ListView.jsx
@@ -3,26 +3,58 @@ import TableRow from "./TableRow";
 
 export default function ListView({ items }) {
   const [data, setData] = useState(items);
-  function compareBy(key) {
-    return function (a, b) {
-      if (a[key] < b[key]) return -1;
-      if (a[key] > b[key]) return 1;
-      return 0;
-    };
+  // TODO title and released need separate sort values
+  // TODO when one is clicked, the other needs to be cleared
+  // TODO Right now they just toggle together (ew)
+  const [sortValue, setSortValue] = useState(true);
+
+  const ascending = <span>&#x25B4;</span>;
+  const descending = <span>&#x25BE;</span>;
+
+  function compareBy(key, sortOrder) {
+    if (sortOrder === true) {
+      console.log("Ascending");
+      return function (a, b) {
+        if (a[key] < b[key]) return -1;
+        if (a[key] > b[key]) return 1;
+        return 0;
+      };
+    } else {
+      console.log("Descending");
+      return function (a, b) {
+        if (a[key] < b[key]) return 1;
+        if (a[key] > b[key]) return -1;
+        return 0;
+      };
+    }
   }
 
-  function sortBy(key) {
+  function sortBy(key, sortOrder) {
     let arrayCopy = [...items];
-    arrayCopy.sort(compareBy(key));
+    arrayCopy.sort(compareBy(key, sortOrder));
     setData(arrayCopy);
   }
 
   return (
     <div className="table">
       <div className="header">
-        <div onClick={() => sortBy("title")}>Title</div>
-        <div onClick={() => sortBy("release_date")}>Released</div>
-        <div onClick={() => sortBy("overview")}>Description</div>
+        <div
+          onClick={() => {
+            setSortValue(!sortValue);
+            sortBy("title", sortValue);
+          }}
+        >
+          Title {sortValue ? ascending : descending}
+        </div>
+        <div
+          onClick={() => {
+            setSortValue(!sortValue);
+            sortBy("release_date", sortValue);
+          }}
+        >
+          Released {sortValue ? ascending : descending}
+        </div>
+        <div>Description</div>
       </div>
       <div className="body">
         {data.map((movie, k) => (
