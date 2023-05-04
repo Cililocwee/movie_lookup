afbe8c8aee5064b5b0c58978dc97216ec85edfe2
Corrie Stroup <Corrie.Stroup@gmail.com>
2023-05-04 16:36:07 +0700

    CSS cleanup and modal styling
    Worked on getting the modal to fit with the overall theme of the application

--- src/components/ShowWatchListButton.jsx
+++ src/components/ShowWatchListButton.jsx
@@ -47,22 +47,29 @@ export default function ShowWatchListButton() {
   return (
     <div>
       <dialog id="watch-list-modal">
-        <button id="watch-list-modal-close">Close</button>
-        <button id="watch-list-clear" onClick={handleRemoveAll}>
-          Clear List
-        </button>
-        {currentWatchList.length > 0 ? (
-          currentWatchList.map((item) => (
-            <div className="watch-list-mini-card">
-              <RemoveMovieButton title={item[0]} update={removeFromWatchList} />{" "}
-              <p>{item[0]}</p>
-            </div>
-          ))
-        ) : (
-          <p>
-            <strong>There are no movies on your Watch List :(</strong>
-          </p>
-        )}
+        <div id="watch-list-background">
+          <div id="watch-list-body">
+            <button id="watch-list-modal-close">Close</button>
+            <button id="watch-list-clear" onClick={handleRemoveAll}>
+              Clear List
+            </button>
+            {currentWatchList.length > 0 ? (
+              currentWatchList.map((item) => (
+                <div className="watch-list-mini-card">
+                  <RemoveMovieButton
+                    title={item[0]}
+                    update={removeFromWatchList}
+                  />{" "}
+                  <p>{item[0]}</p>
+                </div>
+              ))
+            ) : (
+              <p>
+                <strong>There are no movies on your Watch List :(</strong>
+              </p>
+            )}
+          </div>
+        </div>
       </dialog>
       <button id="watch-list-button" onClick={handleClick}>
         Watch List
