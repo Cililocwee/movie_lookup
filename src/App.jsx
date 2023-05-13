ba397161d09442b04eefef79cc1e85dde123d6f1
Corrie Stroup <Corrie.Stroup@gmail.com>
2023-05-13 19:29:33 +0700

    Added toasts on successfully adding movie to WatchList

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import "./styles.scss";
 import MovieFinder from "./components/MovieFinder";
 import Footer from "./components/Footer";
 import ThemeToggle from "./components/ThemeToggle";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <div className="App">
+      <ToastContainer />
       {/* <ThemeToggle /> */}
       <MovieFinder />
       <Footer />
--- src/components/AddToWatchButton.jsx
+++ src/components/AddToWatchButton.jsx
@@ -1,11 +1,30 @@
 import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function AddToWatchButton({ movieBundle }) {
+  const notify = () =>
+    toast.success(
+      `${movieBundle[0]} (${movieBundle[1]}) added to Watch List!`,
+      {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      }
+    );
+
   function saveToList() {
     checkLocalStorage();
     let tempList = JSON.parse(localStorage.getItem("MovieHoundWatchList"));
     tempList.push(movieBundle);
     localStorage.setItem("MovieHoundWatchList", JSON.stringify(tempList));
+    notify();
+    console.log(movieBundle);
   }
 
   function checkLocalStorage() {
@@ -16,9 +35,11 @@ export default function AddToWatchButton({ movieBundle }) {
     }
   }
   return (
-    <button
-      className="add-to-watchlist-button"
-      onClick={() => saveToList()}
-    ></button>
+    <div>
+      <button
+        className="add-to-watchlist-button"
+        onClick={() => saveToList()}
+      ></button>
+    </div>
   );
 }
